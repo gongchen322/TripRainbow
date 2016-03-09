ac13375980306474f1175e83e4ed6c3cd5e979fa
gongchen322 <gong27@purdue.edu>
2016-03-09 15:11:31 -0800

    Data analysis

--- js/controllers/costController.js
+++ js/controllers/costController.js
@@ -4,6 +4,38 @@
       $scope.name="";
       $scope.description="";
       $scope.money="";
+      $scope.total = 0;
+      $scope.population = 0;
+      $scope.average = 0;
+      $scope.individualCosts={};
+      $scope.records=[];
+      $scope.needToPay = 0;
+      $scope.setData = (function bar() {
+       var req = {
+            method: 'GET',
+            url: '/data',
+      }; 
+      $http(req).success(function(data, status) {
+          console.log("Set Data");
+          $scope.records = data;
+          console.log($scope.records);
+          $scope.total = 0;
+          for(var i=0;i<$scope.records.length;i++) {
+            $scope.total += parseInt($scope.records[i].money)
+          }
+          $scope.individualCosts = {};
+          for(var i=0;i<$scope.records.length;i++) {
+            if($scope.individualCosts[$scope.records[i].name]!=null)
+              $scope.individualCosts[$scope.records[i].name]+= parseInt($scope.records[i].money);
+            else
+              $scope.individualCosts[$scope.records[i].name] = parseInt($scope.records[i].money);
+          }
+          console.log(Object.keys($scope.individualCosts));
+          $scope.population = Object.keys($scope.individualCosts).length;
+          $scope.average = Math.round(($scope.total) / $scope.population);
+        });  
+        return bar;
+      })();
       $scope.addToSystem = function () {
         //dataService.addEntry($scope.name,$scope.description,$scope.money);
         var body = {
@@ -17,28 +49,22 @@
               data: JSON.stringify(body)
         }; 
         $http(req).success(function(data, status) {
-            console.log("Successfully save data");
-           var req = {
+          console.log("Successfully save data");
+          var req = {
               method: 'GET',
               url: '/data',
-        }; 
-        $http(req).success(function(data, status) {
+          }; 
+          $http(req).success(function(data, status) {
             console.log("Successfully get data");
-            $scope.records = data;     
+            $scope.records = data;
+            $scope.setData();
           });      
-          });       
+        });       
       }
 
-      $scope.setData = (function () {
-         var req = {
-              method: 'GET',
-              url: '/data',
-        }; 
-        $http(req).success(function(data, status) {
-            console.log("Successfully get data");
-            $scope.records = data;     
-          });  
-      })();
+
+
+
     };
 
     costController.$inject = ['$scope','$http','$state','dataService'];
